53efca2d211d92680eabca8e4d3f435d23cb1a07
Aarushamit <64181805+Aarushamit@users.noreply.github.com>
2020-11-19 21:33:11 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXhcmqnM7tqgPB9w9QCDb9a8x9TxkHK3U",
+  authDomain: "barterapp-1.firebaseapp.com",
+  databaseURL: "https://barterapp-1.firebaseio.com",
+  projectId: "barterapp-1",
+  storageBucket: "barterapp-1.appspot.com",
+  messagingSenderId: "702399188599",
+  appId: "1:702399188599:web:a4f8f8d1efbed416b38072",
+  measurementId: "G-P3S49VWQ13"
+};
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
